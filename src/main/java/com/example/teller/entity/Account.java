c74f5f4822986568d8b3aa0d72d30965c4d48cf3
fis2vcb <nguyenha.cntt@outlook.com>
2022-01-21 17:33:18 +0700

    add project

---
+++ src/main/java/com/example/teller/entity/Account.java
@@ -0,0 +1,8 @@
+package com.example.teller.entity;
+
+public class Account {
+    private Long id;
+    private Long cif;
+    private Float balance;
+    private LocalDate dob;
+}
---
+++ src/main/java/com/example/teller/entity/Customer.java
@@ -0,0 +1,10 @@
+package com.example.teller.entity;
+
+import java.time.LocalDate;
+
+public class Customer {
+    private Long id;
+    private Long sid;
+    private String name;
+    private LocalDate dob;
+}
