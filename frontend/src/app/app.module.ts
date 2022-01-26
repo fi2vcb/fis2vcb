a57518251d6710357d25c8f9789927366e75ea00
bachkhanh <bachkhanh24@gmail.com>
2022-01-26 17:12:01 +0700

    prepare for merging

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,20 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CIFQueryComponent } from './cif-query/cif-query.component';
-import { TransferTrongvxComponent } from './transfer-trongvx/transfer-trongvx.component'; 
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { CreateAccountComponent } from './create-account/create-account.component'; 
 
 
 const routes: Routes = [
   { path: 'QueryAccount', component: CIFQueryComponent },
-  { path: 'Transfer', component: TransferTrongvxComponent }
+  { path: 'CreateAccount', component: CreateAccountComponent }
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    CIFQueryComponent,
-    TransferTrongvxComponent
+    CIFQueryComponent
   ],
   imports: [
     BrowserModule,
