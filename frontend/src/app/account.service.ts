52d882d875c888b22c2f72c5eacbf7d8611bedcc
bachkhanh <bachkhanh24@gmail.com>
2022-02-08 16:44:30 +0700

    add extra files to git

---
+++ frontend/src/app/account.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Account } from './account';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private apiServiceURL = environment.apiBaseURL;
+  constructor(private http: HttpClient) { }
+
+  public addAccount(account: Account): Observable<Account> {
+    return this.http.get<Account>(`${this.apiServiceURL}/api/v1/AddAccount`);
+  }
+
+  public getAccounts(): Observable<Account[]> {
+    return this.http.get<Account[]>(`${this.apiServiceURL}/api/v1/ShowAccounts`);
+  }
+
+  public transferAccounts(fromAccount: Account, toAccount: AccountService, transfer: number): Observable<void> {
+    return this.http.get<void>(`${this.apiServiceURL}/api/v1/Transfer`);
+  }
+
+  public findAccount(cif: string): Observable<Account> {
+    return this.http.get<Account>(`${this.apiServiceURL}/api/v1/QueryAccount/${cif}`);
+  }
+}
---
+++ frontend/src/app/account.ts
@@ -0,0 +1,6 @@
+export interface Account{
+    id: number;
+    cif: number;
+    balance: number;
+    dob: Date;
+}
