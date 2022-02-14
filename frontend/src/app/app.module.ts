2b17c4bc251ef142a934b1985885b6f3f9ea80d3
Sylph08 <trongvuxuan1998@gmail.com>
2022-02-14 10:33:18 +0700

    release 1.0

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TransferComponent } from './transfer/transfer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: '', component: TransferComponent }
+]
 
 @NgModule({
   declarations: [
@@ -11,8 +18,11 @@ import { TransferComponent } from './transfer/transfer.component';
     TransferComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/transfer/transfer.component.ts
+++ frontend/src/app/transfer/transfer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Transaction } from '../common/transaction';
+import { TransferServiceService } from '../services/transfer-service.service';
 
 @Component({
   selector: 'app-transfer',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transfer.component.css']
 })
 export class TransferComponent implements OnInit {
-
-  constructor() { }
+  mainFormGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder,
+    private transferService: TransferServiceService) { }
 
   ngOnInit(): void {
+    this.mainFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        tkgui: new FormControl('', []),
+        tknhan: new FormControl('', []),
+        sotien: new FormControl('', [])
+      })
+    })
+  }
+
+  get tkgui() { return this.mainFormGroup.get('customer.tkgui'); }
+  get tknhan() { return this.mainFormGroup.get('customer.tknhan'); }
+  get sotien() { return this.mainFormGroup.get('customer.sotien'); }
+
+  onSubmit() {
+    console.log("Handling the submit button");
+    let trans = new Transaction();
+    trans.tkgui = this.mainFormGroup.get('customer.tkgui').value;
+    trans.tknhan = this.mainFormGroup.get('customer.tknhan').value;
+    trans.sotien = this.mainFormGroup.get('customer.sotien').value;
+    this.transferService.startTransfer(trans).subscribe({
+      next: res => { alert('Success!'); this.resetMainForm(); },
+      error: res => { alert('Error!'); }
+    })
+  }
+  resetMainForm() {
+    this.mainFormGroup.reset();
   }
 
 }
